99cc0eea394aeaecbc98bd0f84feecae377490f0
Eshayat <133668606+ESHAYAT102@users.noreply.github.com>
2025-02-02 21:06:40 +0600

    Add files via upload

---
+++ dist/_next/static/chunks/619edb50-eb83aba70210ed11.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[271],{13117:(h,t,l)=>{l.d(t,{T69:()=>e});var a=l(93435);function e(h){return(0,a.k5)({tag:"svg",attr:{version:"1.1",viewBox:"0 0 16 16"},child:[{tag:"path",attr:{d:"M5.032 13l0.9-3h4.137l0.9 3h1.775l-3-10h-3.488l-3 10h1.776zM7.432 5h1.137l0.9 3h-2.937l0.9-3z"},child:[]}]})(h)}}}]);
